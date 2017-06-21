adcfb7dc6325fa07e1ed39127d56a079c55ee226
valentin.le-fevre@ens-lyon.fr <valentin.le-fevre@ens-lyon.fr>
2017-06-21 17:48:54 +0200

    gem5 and mcpat files + converter

---
+++ gem5/vector_mul.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	vector<double> v;
+	for (int i=0; i<100; i++)
+	{
+		v.push_back((double)(i+1)/20.);
+	}
+	double total = 1;
+	for (int i=0; i<100; i++)
+	{
+		total *= v[i];
+	}
+	cout << total << "\n";
+	return 0;
+}
