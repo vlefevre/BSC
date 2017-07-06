c8c76a3ef051ab96ce1d39888bc3fc0273807b0e
valentin.le-fevre@ens-lyon.fr <valentin.le-fevre@ens-lyon.fr>
2017-07-06 12:28:03 +0200

    analyzer: titles and legend

--- AMG2006/test/edit/analyzer.py
+++ AMG2006/test/edit/analyzer.py
@@ -6,6 +6,7 @@ from os import listdir
 from os.path import isfile, join
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import numpy as np
 
 #Default parameters
@@ -15,7 +16,7 @@ tolerance = 1e-8
 alpha = 2.0
 exe="./time_estimator"
 
-colors={3:'red',2:'blue',1:'black',4:'green',7:'purple',13:'yellow',14:'orange',17:'cyan',46:'#999999',5:'#00CC55'}
+colors={1:'black',2:'blue',3:'red',4:'green',5:'#00CC55',7:'purple',13:'yellow',14:'orange',17:'cyan',46:'#999999'}
 
 #Parse command-line
 try:
@@ -44,7 +45,10 @@ src_dir = "../PREC_INCR_"+str(thr)+"/evaluation/"
 result = []
 files = [f for f in listdir(src_dir) if isfile(join(src_dir, f))]
 
-i=0
+print "Found "+str(len(files))+" files."
+
+minVal=100000000
+minLabel=""
 for f in files:
 	ftab = f.split("_")
 	precision = int(ftab[1])
@@ -57,7 +61,7 @@ for f in files:
 		if precision > max_prec:
 			precision = max_prec
 		labeltab += [precision]
-	print len(labeltab)
+	#print len(labeltab)
 	#retrieve cost of reaching tolerance under model alpha
 	cmd_string = exe+" "+join(src_dir,f)+" tmp "+str(alpha)
 	system(cmd_string)
@@ -65,12 +69,15 @@ for f in files:
 	data = np.loadtxt("tmp")
 	data = data[data[:,3]<tolerance]
 	value=data[0,2]
-	print value
+	#print value
 	#label = ",".join(str(x) for x in labeltab)
 	label = "_".join(ftab[1:])
 	result.append([label,value,len(labeltab)])
-	i = i+1
-print "Found "+str(i)+" files."
+	if value < minVal:
+		minVal = value
+		minLabel = label
+
+print "Minimum time is "+str(minVal)+", for "+minLabel+"."
 
 result.sort(key=lambda x: int((x[0].split("_"))[0])) #sort by increasing starting precision
 #result.sort(key=lambda x: x[1]) #sort by increasing time
@@ -80,12 +87,17 @@ cval = np.array(resultplot[:,2],dtype='i8')
 bar = plt.bar(np.arange(0,len(result)),yval,0.4)
 i=0
 for column in bar:
-	print cval[i]
-	print colors[cval[i]]
 	column.set_color(colors[cval[i]])
 	i = i+1
-
+handles = []
+for key in sorted(colors):
+	rpatch = mpatches.Patch(color=colors[key], label=str(key))
+	handles.append(rpatch)
+plt.legend(handles=handles,ncol=(len(colors)+1)/2,prop={'size':10},title='Number of precisions used')
+plt.ylabel("Units of time")
+plt.xlabel("Precision sets")
 plt.xticks(pylab.arange(0,len(result)),resultplot[:,0],rotation='vertical')
+plt.title(r"Time to reach accuracy "+str(tolerance)+"\nfor different strategies with threshold "+str(thr)+"\nand time model is $T(b)=b^{"+str(alpha)+"}$.")
 plt.tight_layout()
 
 output_file = "timemodel_"+str(thr)+"_"+str(tolerance)+"_"+str(alpha)+".pdf"
